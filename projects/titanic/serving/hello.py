1b19e93b0c52fe1a6e587aa631647934746de0f3
Mikael Souza <mikael.souza.cc@gmail.com>
2023-09-07 22:35:12 -0400

    fmt files

--- projects/titanic/serving/hello.py
+++ projects/titanic/serving/hello.py
@@ -1,4 +1,10 @@
 import polars
 
-data = polars.DataFrame([1,2,3])
+
+def func(a: polars.DataFrame, b: polars.DataFrame) -> polars.DataFrame:
+    return a + b
+
+
+data = polars.DataFrame([1, 2, 3])
+func(data, data)
 print(data)
